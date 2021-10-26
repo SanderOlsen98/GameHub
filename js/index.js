0a00616129891851a1816fef72c2f58ccba5a4b5
SanderOlsen98 <sanderlolsen@gmail.com>
2021-10-26 21:07:41 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://sanderolsentestsites.one/wp-json/wc/store/products";
+const baseUrl = "https://sanderolsentestsites.one/wp-json/wc/store/products/";
 const productContainer = document.querySelector("#customID");
 
 async function getProducts(baseUrl) {
@@ -7,7 +7,9 @@ async function getProducts(baseUrl) {
     products.forEach(function(product){
         productContainer.innerHTML += `
         <div class="products">
+        <a href="details.html?game=${product.name}">
         <img src="${product.images[0].src}">
+        </a>
         <h3>${product.name}</h3>
         <a class="add-cart price" onclick="alertbox()">${product.prices.price}$</a>
         </div>`;
